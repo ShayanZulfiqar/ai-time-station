f481ad284408aac0079571b5ea8621b7dc497ace
Shayan Zulfiqar <146065078+ShayanZulfiqar@users.noreply.github.com>
2024-11-11 17:40:14 +0400

    Shayan || Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,75 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import logo from '../assets/images/logo.png';
 import Button from './Button';
-import logo from '../assets/images/logo.png'
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
-    const [isDarkMode, setIsDarkMode] = useState(false);
 
-    // Toggle the hamburger menu
     const toggleMenu = () => setIsOpen(!isOpen);
 
-    // Toggle Dark Mode
-    const toggleDarkMode = () => {
-        setIsDarkMode(!isDarkMode);
-    };
-
-    // Apply the dark mode class to the body element on component mount
-    useEffect(() => {
-        if (isDarkMode) {
-            document.body.classList.add('dark');
-        } else {
-            document.body.classList.remove('dark');
-        }
-    }, [isDarkMode]);
-
     return (
-        <nav className="bg-gray-100 dark:bg-gray-900 p-4">
-            <div className="flex justify-between items-center mx-auto container">
-                <div className="font-bold text-blue-900 dark:text-white text-lg">
-                <a href="/"><img src={logo} width={'130px'} alt="" /></a>
-                </div>
+        <nav className="bg-light p-4">
+            {/* Large Screen Navbar */}
+            <div className="hidden lg:flex container mx-auto justify-between items-center">
+                {/* Logo */}
+                <a href="/" className="text-lg font-bold text-blue-900">
+                    <img src={logo} width="130px" alt="Logo" />
+                </a>
 
-                {/* Hamburger Icon for mobile view */}
+                {/* Desktop Menu */}
+                <div className="flex items-center space-x-4">
+                    <a
+                        href="/"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
+                    >
+                        Home
+                    </a>
+                    <a
+                        href="/HowItWork"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
+                    >
+                        How it Works
+                    </a>
+                    <a
+                        href="/Pricing"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
+                    >
+                        Pricing
+                    </a>
+                    <a
+                        href="/signup"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
+                    >
+                        Signup
+                    </a>
+                    <a
+                        href="/Download"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
+                    >
+                        Download
+                    </a>
+                    <a
+                        href="/Support"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
+                    >
+                        Support
+                    </a>
+
+                    {/* Login Button */}
+                    <Button text="Login" link="/Login" />
+                </div>
+            </div>
+
+            {/* Mobile Screen Navbar */}
+            <div className="lg:hidden container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <a href="/" className="text-lg font-bold text-blue-900">
+                    <img src={logo} width="130px" alt="Logo" />
+                </a>
+
+                {/* Hamburger Button */}
                 <button
-                    className="lg:hidden text-gray-800 dark:text-white"
+                    className="text-gray-800"
                     onClick={toggleMenu}
                 >
                     <svg
@@ -50,92 +87,52 @@ const Navbar = () => {
                         />
                     </svg>
                 </button>
+            </div>
 
-                {/* Links container */}
-                <div
-                    className={`${isOpen ? 'block' : 'hidden'
-                        } lg:flex flex-col lg:flex-row items-center space-x-4 mt-4 lg:mt-0`}
-                >
+            {/* Mobile Menu (when the hamburger button is clicked) */}
+            {isOpen && (
+                <div className="lg:hidden mt-10 z-50 flex flex-col items-center space-y-4 absolute top-16 left-0 w-full bg-white shadow-lg py-4">
                     <a
                         href="/"
-                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transform transition hover:scale-105"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
                     >
                         Home
                     </a>
                     <a
                         href="/HowItWork"
-                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transform transition hover:scale-105"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
                     >
                         How it Works
                     </a>
                     <a
                         href="/Pricing"
-                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transform transition hover:scale-105"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
                     >
                         Pricing
                     </a>
                     <a
                         href="/signup"
-                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transform transition hover:scale-105"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
                     >
                         Signup
                     </a>
                     <a
                         href="/Download"
-                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transform transition hover:scale-105"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
                     >
                         Download
                     </a>
                     <a
                         href="/Support"
-                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transform transition hover:scale-105"
+                        className="hover:bg-blue-700 p-2 rounded-md font-semibold hover:text-white transition duration-300"
                     >
                         Support
                     </a>
+
+                    {/* Login Button */}
+                    <Button text="Login" link="/Login" />
                 </div>
-
-                
-                <Button text="Login" link="/Login" />
-
-                {/* Dark Mode Toggle Button */}
-                {/* <button
-                    onClick={toggleDarkMode}
-                    className="ml-4 p-2 text-white bg-gray-700 rounded-full hover:bg-gray-600 transition-colors"
-                >
-                    {isDarkMode ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M12 3v2m0 14v2m9-9h2M3 12H1m15.364-6.364l1.414 1.414M5.636 18.364l-1.414-1.414M16.243 16.243l1.414 1.414M7.757 7.757L6.343 6.343"
-                            />
-                        </svg>
-                    ) : (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            className="w-6 h-6"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M12 3v2m0 14v2m9-9h2M3 12H1m15.364-6.364l1.414 1.414M5.636 18.364l-1.414-1.414M16.243 16.243l1.414 1.414M7.757 7.757L6.343 6.343"
-                            />
-                        </svg>
-                    )}
-                </button> */}
-
-            </div>
+            )}
         </nav>
     );
 };
--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -1,25 +1,14 @@
 import React from "react";
 import Button from "./Button";
+import './PromoBanner.css'
 
 const PromoBanner = () => {
     return (
-        <div
-            className="relative w-full h-[400px] lg:h-[500px]" // Removed conflicting Tailwind classes
-            style={{
-                backgroundImage: `url('../assets/images/Home_Banner_02.jpg')`, // Corrected image path
-                height: '450px',
-                width: '100%',
-                backgroundSize: 'cover',        // Ensures the image covers the entire div
-                backgroundPosition: 'center',   // Centers the image
-                backgroundRepeat: 'no-repeat',  // Prevents the image from repeating
-                backgroundAttachment: 'scroll', // Adjust as needed (scroll or fixed)
-                backgroundColor: 'transparent', // Optional fallback color
-            }}
-        >
+        <div className="promo-banner">
             {/* Dark Overlay */}
-            <div className="absolute inset-0 flex justify-start items-center bg-black bg-opacity-40">
+            <div className="absolute inset-0 flex justify-start items-center">
                 {/* Text and Button Container */}
-                <div className="space-y-6 ml-8 lg:ml-20 p-8 max-w-lg text-white">
+                <div className="space-y-6 ml-8 lg:ml-20 p-8 max-w-lg bg-slate-200 rounded-xl ">
                     <h2 className="font-bold text-2xl lg:text-4xl">
                         Turn any smartphone or tablet into a cloud-based time & attendance
                         system
