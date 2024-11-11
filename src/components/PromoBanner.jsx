04e1f9814ce49341b837a0870dc32db9a18ee0f0
Shayan Zulfiqar <146065078+ShayanZulfiqar@users.noreply.github.com>
2024-11-11 18:23:05 +0400

    Shayan || Udate || All pages

--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -4,11 +4,11 @@ import './PromoBanner.css'
 
 const PromoBanner = () => {
     return (
-        <div className="promo-banner">
+        <div className="promo-banner relative">
             {/* Dark Overlay */}
-            <div className="absolute inset-0 flex justify-start items-center">
+            <div className="absolute inset-0 bg-black bg-opacity-50 px-8 md:px-1 flex justify-start items-center">
                 {/* Text and Button Container */}
-                <div className="space-y-6 ml-8 lg:ml-20 p-8 max-w-lg bg-slate-200 rounded-xl ">
+                <div className="space-y-6 md:ml-20 ml-0 p-8 max-w-lg bg-slate-200 rounded-xl w-full lg:w-auto mx-auto">
                     <h2 className="font-bold text-2xl lg:text-4xl">
                         Turn any smartphone or tablet into a cloud-based time & attendance
                         system
@@ -17,6 +17,7 @@ const PromoBanner = () => {
                 </div>
             </div>
         </div>
+
     );
 };
 
